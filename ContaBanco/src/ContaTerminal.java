3119d8e945dd7e85a90b204415d8e73cad0a7bc2
vagramos <vag.pare@gmail.com>
2025-07-29 18:17:56 -0300

    Adicionado o mapeamento das tasks.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,14 @@
 public class ContaTerminal {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+        //@TODO:  Conhecer e importar a classe Scanner
+
+        //@TODO:  Exibir as mensagens para nosso usuário
+
+        //@TODO:  Obter pela scanner os valores digitados no terminal
+
+        //@TODO:  Exibir a mensagem da conta criada
     }
 }
