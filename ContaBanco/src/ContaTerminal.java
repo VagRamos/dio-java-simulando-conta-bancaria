959b3677e2dfa4479f7ec4cad0f975cfd257be8f
vagramos <vag.pare@gmail.com>
2025-07-31 00:14:52 -0300

    Entrega do projeto implementado.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,14 +1,34 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Boas vindas ao sistema de cadastro de contas bancárias.\n");
 
-        //@TODO:  Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
 
-        //@TODO:  Exibir as mensagens para nosso usuário
+        System.out.print("Por favor, digite o número de sua conta: (XXXX)\n");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
 
-        //@TODO:  Obter pela scanner os valores digitados no terminal
+        System.out.print("Por favor, digite o número de sua agência: (XXX-X)\n");
+        String numeroAgencia = scanner.nextLine();
 
-        //@TODO:  Exibir a mensagem da conta criada
+        System.out.print("Por favor, digite o nome completo:\n");
+        String nomeCliente = scanner.nextLine().toUpperCase();
+
+        System.out.print("Por favor, digite o saldo da conta:\n");
+        double saldoConta = scanner.nextDouble();
+
+        Locale localeBrasil = new Locale("pt", "BR");
+        NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(localeBrasil);
+        String saldoFormatado = formatoMoeda.format(saldoConta);
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo de %s%n já está disponível para saque.\n",
+                nomeCliente, numeroAgencia, numeroConta, saldoFormatado);
+
+        scanner.close();
     }
 }
